81f5bc5f4cf1cbdbb46207b6010438f5710666b8
Baptiste Mispelon <bmispelon@gmail.com>
2012-05-09 19:58:58 +0200

    Use the fileinput module.

--- parse_log.py
+++ parse_log.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from datetime import datetime as dt
 import httpagentparser
+from itertools import ifilter, imap
 from json import dumps
 from pygeoip import GeoIP
 import re
@@ -9,7 +10,19 @@ from vector_dict.VectorDict import VectorDict as krut, convert_tree as kruter
 
 gi = GeoIP("data/GeoIP.dat")
 
-country = gi.country_code_by_addr
+LOG_LINE_REGEXP = re.compile(
+'''^(?P<ip>\S+?)\s # ip
+-\s                    # dunno
+(?P<user>[^ ]+)\s      # if authentified
+\[(?P<time>[^\]]+)\]\s # apache format
+"(?P<method>[A-Z]+)\   # GET/POST/....
+(?P<uri>[^ ]+)\        # add query it would be nice
+HTTP/1.\d"\            # whole scheme (catching FTP ... would be nicer)
+(?P<status>\d+)\       # 404 ...
+(?P<bytes>\d+)\        # bytes really bite me if you can
+"(?P<referer>[^"]+)"\  # where people come from
+"(?P<agent>[^"]+)"$    # well ugly chain''', re.VERBOSE)
+
 cache = {}
 def memoize_detect(user_agent):
     default = {
@@ -26,59 +39,50 @@ def memoize_detect(user_agent):
     
     return cache[user_agent]
 
-
-def parse(filename):
-    """Return tuple of dictionaries containing file data."""
-    log_re= '''^(?P<ip>\S+?)\s # ip
-        -\s                    # dunno
-        (?P<user>[^ ]+)\s      # if authentified
-        \[(?P<time>[^\]]+)\]\s # apache format
-        "(?P<method>[A-Z]+)\   # GET/POST/....
-        (?P<uri>[^ ]+)\        # add query it would be nice
-        HTTP/1.\d"\            # whole scheme (catching FTP ... would be nicer)
-        (?P<status>\d+)\       # 404 ...
-        (?P<bytes>\d+)\        # bytes really bite me if you can
-        "(?P<referer>[^"]+)"\  # where people come from
-        "(?P<agent>[^"]+)"$    # well ugly chain'''
-    search = re.compile(log_re, re.X).search
-    with open(filename) as f:
-        for line in f:
-            match = search(line)
-            if match:
-                res = match.groupdict()
-                res["fdate"] = dt.strptime(
-                     res["time"][:-6],
-                    "%d/%b/%Y:%H:%M:%S"
-                ).isoformat()
-                res["date"] = res["fdate"][:11]
-                res["time"] = res["fdate"][11:]
-                res["agent_class"] = memoize_detect(res["agent"])
-                yield res
+def parse_line(line):
+    """Produce a dict of the data found in the line.
+    If the line is not recognized, return None.
+    
+    """
+    match = LOG_LINE_REGEXP.search(line)
+    if not match:
+        return None
+    data = match.groupdict()
+    fdate = dt.strptime(data["time"][:-6], "%d/%b/%Y:%H:%M:%S")
+    data.update({
+        "date": fdate.strftime('%Y-%m-%d'),
+        "time": fdate.strftime('%H:%M:%S.%f'),
+        "agent_class": memoize_detect(data["agent"])
+    })
+    return data
 
 if __name__ == '__main__':
-    from sys import argv
+    import fileinput
+    
+    def krutify(data):
+        return krut(int, {
+            "by_country": krut(int, {gi.country_code_by_addr(data['ip']): 1}),
+            "by_date": krut(int, {data["date"]: 1 }),
+            "by_hour": krut(int, {data["time"][0:2]: 1 }),
+            "by_os": krut(int, {data["agent_class"]['os']['name']: 1 }),
+            "by_dist": krut(int, {data["agent_class"]['dist']['name']: 1 }),
+            "by_browser": krut(int, {data["agent_class"]['browser']['name']: 1 }),
+            "by_ip": krut(int, {data['ip']: 1 }),
+            "by_status": krut(int, {data['status']: 1 }),
+            "by_url": krut(int, {data['uri']: 1}),
+            "by_agent": krut(int, {data['agent']: 1}),
+            "ip_by_url": krut(int, {data['uri']: krut (int, {data['ip']: 1 })}),
+            "bytes_by_ip": krut(int, {data['ip']: int(data["bytes"])})
+        })
+    
+    def is_local(ip):
+        return ip.startswith('192.168')
     
     reduce(
         krut.__add__,
-        map(
-            lambda x : krut(int, {
-                "by_country": krut(int, {country(x['ip']): 1}),
-                "by_date": krut(int, {x["date"]: 1 }),
-                "by_hour": krut(int, {x["time"][0:2]: 1 }),
-                "by_os": krut(int, {x["agent_class"]['os']['name']: 1 }),
-                "by_dist": krut(int, {x["agent_class"]['dist']['name']: 1 }),
-                "by_browser": krut(int, {x["agent_class"]['browser']['name']: 1 }),
-                'by_ip': krut(int, {x['ip']: 1 }),
-                'by_status': krut(int, {x['status']: 1 }),
-                'by_url': krut(int, {x['uri']: 1}),
-                'by_agent': krut(int, {x['agent']: 1}),
-                'ip_by_url': krut(int, {x['uri']: krut (int, {x['ip']: 1 })}),
-                "bytes_by_ip": krut(int, {x['ip']: int(x["bytes"])})
-                }
-                ),
-            filter(
-                lambda x: not x['ip'].startswith('192.168'),
-                parse(argv[1])
+        imap(krutify, ifilter(
+                lambda x: not is_local(x['ip']),
+                ifilter(None, imap(parse_line, fileinput.input()))
             )
         )
     ).tprint()
