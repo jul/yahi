6809d44fc61629764372a608ba48094a2010bf35
jul <julien@tayon.net>
2012-05-09 15:45:02 +0200

    bmispelon powa

--- parse_log.py
+++ parse_log.py
@@ -8,23 +8,13 @@ country = gi.country_code_by_addr
 
 def parse(filename):
     'Return tuple of dictionaries containing file data.'
-    def make_entry(x):
-        return {
-            'ip':x.group('ip'),
-            'user' : x.group('user'),
-            'uri':x.group('uri'),
-            "bytes":x.group("bytes"),
-            'time':x.group('time'),
-            'status_code':x.group('status_code'),
-            'referer':x.group('referer'),
-            'agent':x.group('agent'),
-            }
-  #  log_re = r'(?P<ip>.*?) -(?P<user>.*?)- \[(?P<time>.*?)\] "(?P<method>.*?) (?P<uri>.*?) HTTP/1.\d" (?P<status_code>\d*) (?P<bytes>.*?) "(?P<referral>.*?)" "(?P<agent>.*?)"'
     log_re= '^(?P<ip>.*?) - (?P<user>\S+) \[(?P<time>.*?)\] "(?P<method>.*?) (?P<uri>.*?) HTTP/1.\d" (?P<status_code>\d+) (?P<bytes>\d+) "(?P<referer>.*?)" "(?P<agent>.*?)'
     search = re.compile(log_re).search
-    matches = (search(line) for line in open(filename))
-#    print [ x and  x.group('ip') or x  for x in matches  ]
-    return [make_entry(x)  for x in matches if x  ]
+    with open(filename) as f:
+        for line in f:
+            match = search(line)
+            if match:
+                yield match.groupdict()
 
 from json import dumps
 from sys import argv
