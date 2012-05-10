00c314dc95c9f03fab6e024be745c1f2babf5583
jul <julien@tayon.net>
2012-05-10 01:04:49 +0000

    web server

---
+++ www/wwwstat.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, url_for, redirect,flash, safe_join
+from flask import jsonify
+from flaskext.cache import Cache
+from json import dumps
+
+app = Flask(__name__,) 
+cache = Cache(app)
+
+from os import path
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/key/<package>", methods=['GET', 'POST'])
+def key(package):
+    pass
+
+@app.route("/data/", methods=['GET'])
+def data():
+    return redirect(url_for('static', filename="data.json"))
+
+if __name__ == "__main__":
+    app.run(host='192.168.0.5', port = 5001, debug=True)
+
