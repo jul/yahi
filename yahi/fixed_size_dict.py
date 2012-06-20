bbd33f48ce96f585fa5aff1ecc7b1a099c4b3398
julien tayon <jtayon@gmail.com>
2012-06-20 16:12:16 +0200

    safeguard added

--- yahi/fixed_size_dict.py
+++ yahi/fixed_size_dict.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+class SafeGuard:
+    def pop(self,*a,**kw):
+        raise Exception("Unsafe Method")
+
+    def popitem(self,*a,**kw):
+        raise Exception("Unsafe Method")
+
+    def update(self,*a,**kw):
+        raise Exception("Unsafe Method")
+
+    def 
 
 class Sentinel:
     pass
-null = Sentinel()
-class FixedSizeDict(dict):
-    def __init__(self,size):
-        self._key=[ null ] * size
+NULL = Sentinel()
+
+class FixedSizeDict(SafeGuard):
+    def __init__(self,size, factory=dict):
+        self._key=[ NULL ] * size
         self._cursor=0
         self._size=size
-        dict.__init__({})
+        factory.__init__({})
 
     def __setitem__(self,key, value):
         self._cursor +=1
         self._cursor %= self._size
-        if self._key[self._cursor] is not null:
+        if self._key[self._cursor] is not NULL:
             del self[self._key[self._cursor]]
         self._key[self._cursor]=key
         dict.__setitem__(self, key,value)
 
-    def update(self,*a,**kw):
-        raise Exception("This shall not pass")
 
+from collections import deque
+class bmFixedSizeDict(SafeGuard):
+    def __init__(self, maxsize, factory =dict):
+        self._keys = deque([SENTINEL] * maxsize)
+        self.maxsize = maxsize
+        factory.__init__({})
+        # TODO: trim the fat (maybe using dict.popitem)
+
+    def __setitem__(self, key, value):
+        oldkey = self._keys.pop(0)
+        if oldkey is not SENTINEL:
+            del self[oldkey]
+        self._keys.append(key)
+        self[key] = value
