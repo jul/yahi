b7359b69dfcebd6a52968b1d0a84c686dff99b00
julien tayon <jtayon@gmail.com>
2012-06-20 15:39:26 +0200

    inlcuding come of bmispelon good ideas

--- yahi/fixed_sized_dict.py
+++ yahi/fixed_sized_dict.py
@@ -3,11 +3,10 @@
 
 class Sentinel:
     pass
-
+null = Sentinel()
 class FixedSizeDict(dict):
     def __init__(self,size):
-        self._null = Sentinel()
-        self._key=[ self._null ] * size
+        self._key=[ null ] * size
         self._cursor=0
         self._size=size
         dict.__init__({})
@@ -15,10 +14,11 @@ class FixedSizeDict(dict):
     def __setitem__(self,key, value):
         self._cursor +=1
         self._cursor %= self._size
-        if self._key[self._cursor] is not  self._null:
+        if self._key[self._cursor] is not null:
             del self[self._key[self._cursor]]
         self._key[self._cursor]=key
         dict.__setitem__(self, key,value)
+
     def update(self,*a,**kw):
         raise Exception("This shall not pass")
 
