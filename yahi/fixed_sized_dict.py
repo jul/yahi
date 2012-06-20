8fb90cfb8c812e36bea86475fe7c351117a14257
julien tayon <jtayon@gmail.com>
2012-06-20 15:33:19 +0200

    some caveat disappeared

--- yahi/fixed_sized_dict.py
+++ yahi/fixed_sized_dict.py
@@ -5,18 +5,20 @@ class Sentinel:
     pass
 
 class FixedSizeDict(dict):
-    def __init__(self,size,_dict={}):
+    def __init__(self,size):
         self._null = Sentinel()
         self._key=[ self._null ] * size
         self._cursor=0
         self._size=size
-        self = dict.__init__(_dict)
+        dict.__init__({})
 
     def __setitem__(self,key, value):
         self._cursor +=1
         self._cursor %= self._size
-        if self._key[self._cursor] != self._null:
+        if self._key[self._cursor] is not  self._null:
             del self[self._key[self._cursor]]
         self._key[self._cursor]=key
         dict.__setitem__(self, key,value)
+    def update(self,*a,**kw):
+        raise Exception("This shall not pass")
 
