48f45e653b8a4043832acbbd511eb0ca779f7198
julien tayon <jtayon@gmail.com>
2012-06-20 21:45:52 +0200

    bzz

--- yahi/fixed_size_lookup.py
+++ yahi/fixed_size_lookup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Fixed size lookup table. 
+A looklup table is like a dict except that if you want to 
+update or set an already existing value, it blows up"""
+
 class SafeGuard:
+    """guarding all entry point we don't want to be visited"""
     def pop(self,*a,**kw):
         raise Exception("Unsafe Method")
 
@@ -16,15 +21,13 @@ class SafeGuard:
     def setdefault(self,*a,**kw):
         raise Exception("Unsafe Method")
         
+__all__=[ 'FixedLookupTable' ]
 
-
-class Sentinel:
-    pass
-NULL = Sentinel()
-
-class FixedLookupTable(dict,SafeGuard):
+SENTINEL = object()
+### my pityfully lame implementation
+class jtFixedLookupTable(dict,SafeGuard):
     def __init__(self,size):
-        self._key=array([ NULL ] * size)
+        self._key=array([ SENTINEL ] * size)
         self._cursor=0
         self._size=size
         dict.__init__({})
@@ -32,24 +35,32 @@ class FixedLookupTable(dict,SafeGuard):
     def __setitem__(self,key, value):
         self._cursor +=1
         self._cursor %= self._size
-        if self._key[self._cursor] is not NULL:
+        if self._key[self._cursor] is not SENTINEL:
             dict.__delitem__(self,self._key[self._cursor])
         self._key[self._cursor]=key
         dict.__setitem__(self,key,value)
 
 
 from collections import deque
-class bmFixedLookupTable(dict,SafeGuard):
+### bmispelon implementation appearing by courtesy
+class FixedLookupTable(dict,SafeGuard):
+    """a circular dict of a fixed size : 
+    if size is greater than its upper bound, new key will replace the older
+    This is *NOT* a LRU cache. This is a FIFO cache
+    """
+
     def __init__(self, maxsize):
-        self._keys = deque([NULL] * maxsize)
+        self._keys = deque([SENTINEL] * maxsize)
         self.maxsize = maxsize
         dict.__init__({})
         # TODO: trim the fat (maybe using dict.popitem)
     
     def __setitem__(self, key, value):
         oldkey = self._keys.popleft()
-        if oldkey is not NULL:
+        if oldkey is not SENTINEL:
         #    print oldkey in self._keys
             dict.__delitem__(self,oldkey)
         self._keys.append(key)
         dict.__setitem__(self,key,value)
+
+bmFixedLookupTable = FixedLookupTable
