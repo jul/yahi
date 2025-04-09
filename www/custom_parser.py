6bed03d935e02ebe7a81b540f30afe29e5f18797
jul <julien@tayon.net>
2025-04-10 00:00:46 +0200

    custom parser working with aforementionned modifications

--- www/custom_parser.py
+++ www/custom_parser.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from archery.bow import Hankyu
+from archery.bow import mdict
 from yahi import notch, shoot
 from time import mktime,strptime, time
 from json import dumps
@@ -17,24 +17,25 @@ option.output_file.write(
     "data=" + dumps(
         shoot(
             option,
-            lambda data : Hankyu({
-                'by_country': Hankyu({data['_country']: 1}),
+            lambda data : mdict({
+                'by_country': mdict({data['_country']: 1}),
+                'date_bandwidth': # mdict({str(data['_datetime']): 1 }),
+                    mdict({str(dt.combine(data["_datetime"],datetime.time(0,0,0)).date()): int(data["bytes"]) }),
 
-                'by_date': # Hankyu({str(data['_datetime']): 1 }),
-                    Hankyu({str(dt.combine(data["_datetime"],datetime.time(0,0,0)).date()): 1 }),
-                'by_hour': Hankyu({"%2d" % int(data['_hour']): 1 }),
-                'by_os': Hankyu({data['_os_name']: 1 }),
-                "by_date_as_ts": Hankyu({
-                    dt.combine(data["_datetime"],datetime.time(0,0,0)).timestamp(): 1 }),
-                'by_dist': Hankyu({data['_dist_name']: 1 }),
-                'by_browser': Hankyu({data['_browser_name']: 1 }),
-                'by_ip': Hankyu({data['ip']: 1 }),
-                'by_status': Hankyu({data['status']: 1 }),
-                'by_url': Hankyu({data['uri']: 1}),
-                'by_agent': Hankyu({data['agent']: 1}),
-                'by_referer': Hankyu({data['referer']: 1}),
-                'ip_by_url': Hankyu({data['uri']: Hankyu( {data['ip']: 1 })}),
-                'bytes_by_ip': Hankyu({data['ip']: int(data['bytes'])}),
+                'date_hit': mdict({str(data['_datetime'].date()): 1 }),
+                    #mdict({str(dt.combine(data["_datetime"],datetime.time(0,0,0)).date()): 1 }),
+                'hour_hit': mdict({"%2d" % int(data['_hour']): 1 }),
+                'hour_bandwidth': mdict({"%2d" % int(data['_hour']): int(data["bytes"]) }),
+                'by_os': mdict({data['_platform_name']: 1 }),
+                'by_dist': mdict({data['_dist_name']: 1 }),
+                'by_browser': mdict({data['_browser_name']: 1 }),
+                'by_ip': mdict({data['ip']: 1 }),
+                'by_status': mdict({data['status']: 1 }),
+                'by_url': mdict({data['uri']: 1}),
+                'by_agent': mdict({data['agent']: 1}),
+                'by_referer': mdict({data['referer']: 1}),
+                'ip_by_url': mdict({data['uri']: mdict( {data['ip']: 1 })}),
+                'bytes_by_ip': mdict({data['ip']: int(data['bytes'])}),
                 'total_line' : 1,
             }),
         ),
