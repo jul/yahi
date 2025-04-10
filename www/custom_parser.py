db8b6cfd6d9ca9174a6ebdb09a63654d03c70ed4
jul <julien@tayon.net>
2025-04-10 11:21:16 +0200

    removing comments (aesthetic)

--- www/custom_parser.py
+++ www/custom_parser.py
@@ -19,13 +19,17 @@ option.output_file.write(
             option,
             lambda data : mdict({
                 'by_country': mdict({data['_country']: 1}),
-                'date_bandwidth': # mdict({str(data['_datetime']): 1 }),
-                    mdict({str(dt.combine(data["_datetime"],datetime.time(0,0,0)).date()): int(data["bytes"]) }),
-
+                'date_bandwidth': 
+                    mdict({
+                        str(data["_datetime"].date()): int(data["bytes"])
+                    }),
                 'date_hit': mdict({str(data['_datetime'].date()): 1 }),
-                    #mdict({str(dt.combine(data["_datetime"],datetime.time(0,0,0)).date()): 1 }),
                 'hour_hit': mdict({"%2d" % int(data['_hour']): 1 }),
-                'hour_bandwidth': mdict({"%2d" % int(data['_hour']): int(data["bytes"]) }),
+                'hour_bandwidth': 
+                    mdict({
+                        "%2d" % int(data['_hour']):
+                        int(data["bytes"]) 
+                    }),
                 'by_os': mdict({data['_platform_name']: 1 }),
                 'by_dist': mdict({data['_dist_name']: 1 }),
                 'by_browser': mdict({data['_browser_name']: 1 }),
