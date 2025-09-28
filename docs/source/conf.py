ff10f5c314d1844ba8e77210d42e7bacb4fc9d22
jul <julien@tayon.net>
2025-09-28 18:35:07 +0200

    semver bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ copyright = u'2012, julien tayon'
 # The short X.Y version.
 version = "0.3"
 # The full version, including alpha/beta/rc tags.
-release = '0.3.2b'
+release = '0.3.3'
 
 exclude_patterns = []
 
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ long_description = open("README.md").read()
 
 setup(
         name='yahi',
-        version='0.3.2b',
+        version='0.3.3',
         author='Julien Tayon, Stephane Bard',
         author_email='julien@tayon.net',
         packages=['yahi'],
